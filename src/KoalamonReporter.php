603ab738d4ed183d779b538298e37ce27add99e1
Nils Langner <nils.langner@phmlabs.com>
2016-06-24 14:57:21 +0200

    Update KoalamonReporter.php

--- src/KoalamonReporter.php
+++ src/KoalamonReporter.php
@@ -129,7 +129,7 @@ class KoalamonReporter extends Extension
     public function suite()
     {
         $tool = 'Codeception';
-        $koalamonServer = 'http://monitor.koalamon.com';
+        $koalamonServer = 'https://webhook.koalamon.com';
 
         if (array_key_exists('server', $this->config)) {
             $koalamonServer = $this->config['server'];
